0556cd86ee136088f3dd02d2fc9c05768d161af3
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-06-14 14:07:06 +0200

    adapt help command texts

--- lib/help.ts
+++ lib/help.ts
@@ -5,7 +5,7 @@ import { sendNotification } from './sendNotification';
 export function help(context: SlashCommandContext, read: IRead, modify: IModify): void {
     const msg = ['The commands I can understand',
                  'Usage: `/gitlab setup token <your_auth_token>`',
-                 'Usage: `/gitlab create issue <repository id/ path> <issue title> <description>`',
-                 'Usage: `/gitlab search issues <keyword> (optional: <created-by-me>/<assigned-to-me>)`'].join('\n');
+                 'Usage: `/gitlab create issue <repository id/path> <issue title> <description>`',
+                 'Usage: `/gitlab search issues <keyword> (optional: <created-by-me>/<assigned-to-me>/all)`'].join('\n');
     sendNotification(msg, read, modify, context.getSender(), context.getRoom());
 }
