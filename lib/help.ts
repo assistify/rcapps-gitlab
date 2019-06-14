2f5fbd9475ebd9ec8b9befad9443acc639213fc8
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-06-14 15:12:47 +0200

    adapt help command literals

--- lib/help.ts
+++ lib/help.ts
@@ -3,9 +3,10 @@ import { SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashco
 import { sendNotification } from './sendNotification';
 
 export function help(context: SlashCommandContext, read: IRead, modify: IModify): void {
-    const msg = ['The commands I can understand',
-                 'Usage: `/gitlab setup token <your_auth_token>`',
-                 'Usage: `/gitlab create issue <repository id/path> <issue title> <description>`',
-                 'Usage: `/gitlab search issues <keyword> (optional: <created-by-me>/<assigned-to-me>/all)`'].join('\n');
+    const msg = `The commands I can understand:
+                 \`/gitlab setup token <your_auth_token>\`
+                 \`/gitlab create issue <repository id | path> <issue title> <description>\`
+                 \`/gitlab search issues <keyword> (optional: [created-by-me | assigned-to-me | all])\``;
+
     sendNotification(msg, read, modify, context.getSender(), context.getRoom());
 }
