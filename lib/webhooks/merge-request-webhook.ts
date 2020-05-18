992e98bc364e59d13e732813a31150c273e42228
Jonas Michaelis <jonas.michaelis@outlook.com>
2020-05-18 13:18:56 +0200

    add support for merge requests (#7)

--- lib/webhooks/merge-request-webhook.ts
+++ lib/webhooks/merge-request-webhook.ts
@@ -1,5 +1,25 @@
 import { IApiRequest } from '@rocket.chat/apps-engine/definition/api';
 
 export function createMergeRequestMessage(request: IApiRequest): string {
-    return '';
+    const projectUrl = request.content.project.web_url;
+    const repoName = request.content.project.name;
+    const mergeTitle = request.content.object_attributes.title;
+    const mergeUrl = request.content.object_attributes.url;
+
+    const text = `${request.content.user.name} ${getAction(request)} a merge request in repository [${repoName}](${projectUrl})
+        • [${mergeTitle}](${mergeUrl}): ${getDescription(request)}`;
+
+    return text;
+}
+
+function getDescription(request: IApiRequest): string {
+    const issueDescription = request.content.object_attributes.description;
+    if(issueDescription) {
+        return `"${issueDescription}"`;
+    }
+    return '_no description provided_';
+}
+
+function getAction(request: IApiRequest): string {
+    return request.content.object_attributes.state;
 }
--- test/app/suites/merge-request.test.ts
+++ test/app/suites/merge-request.test.ts
@@ -35,3 +35,19 @@ describe('Test Merge Request 2', () => {
         });
     })
 })
+
+describe('Test Merge Request 2', () => {
+    it('send mr hook', async done => {
+        request.post({
+            headers: { 'Content-Type': 'application/json', 'X-Gitlab-Event': 'Merge Request Hook' },
+            url: 'http://localhost:3000/api/apps/public/684202ed-1461-4983-9ea7-fde74b15026c/webhook',
+            body: fs.readFileSync('./json/merge-requests/merge-merge.json')
+        }, async (error, response, body) => {
+            console.log(body)
+            const msg = await messages.getLastMessage();
+            console.log(msg);
+            expect(response.statusCode).toBe(200)
+            done()
+        });
+    })
+})
